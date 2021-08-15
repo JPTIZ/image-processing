875b57c302354023d40901ef16fa6c6a73f658ba
João Paulo Taylor Ienczak Zanette <jpaulotiz@gmail.com>
2021-08-15 17:38:40 -0300

    Make header functions inline

--- src/logger/logger.h
+++ src/logger/logger.h
@@ -21,28 +21,28 @@ constexpr auto MaxLogLevel = LogLevels::Error;
 constexpr auto MaxLogLevel = LogLevels::Debug;
 #endif
 
-void log(std::string_view const& s) {
+inline void log(std::string_view const& s) {
     if constexpr (MaxLogLevel >= LogLevels::Common) {
         fmt::print(fmt::fg(fmt::color::dim_gray), "[LOG] ");
         fmt::print("{}\n", s);
     }
 }
 
-void warn(std::string_view const& s) {
+inline void warn(std::string_view const& s) {
     if constexpr (MaxLogLevel >= LogLevels::Warning) {
         fmt::print(fmt::fg(fmt::color::yellow), "[WRN] ");
         fmt::print("{}\n", s);
     }
 }
 
-void error(std::string_view const& s) {
+inline void error(std::string_view const& s) {
     if constexpr (MaxLogLevel >= LogLevels::Error) {
         fmt::print(fmt::fg(fmt::color::red), "[ERR] ");
         fmt::print("{}\n", s);
     }
 }
 
-void debug(std::string_view const& s) {
+inline void debug(std::string_view const& s) {
     if constexpr (MaxLogLevel >= LogLevels::Debug) {
         fmt::print(fmt::fg(fmt::color::dark_green), "[DBG] ");
         fmt::print("{}\n", s);
